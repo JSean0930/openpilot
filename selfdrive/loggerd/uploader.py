85a442ddf6874ccf50e1f43b0018a82b2ad86e7c
CT921 <tim.chen242@gmail.com>
2022-02-21 21:55:25 +0800

    Update uploader.py

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -237,7 +237,7 @@ def uploader_fn(exit_event):
         time.sleep(60 if offroad else 5)
       continue
 
-    d = uploader.next_file_to_upload()
+    d = None
     if d is None:  # Nothing to upload
       if allow_sleep:
         time.sleep(60 if offroad else 5)
