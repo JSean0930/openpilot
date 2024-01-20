408ca9c47fc35379edcc0ff28cda938cfd506882
CT921 <tim.chen242@gmail.com>
2024-01-20 20:29:37 +0800

    update for TSS2 by Marc

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -282,8 +282,9 @@ class CarInterface(CarInterfaceBase):
     tune.deadzoneBP = [0., 9.]
     tune.deadzoneV = [.0, .15]
     if candidate in TSS2_CAR:
-      ret.vEgoStopping = 0.25
-      ret.vEgoStarting = 0.25
+      ret.stopAccel = -2.0
+      ret.vEgoStopping = 0.1
+      ret.vEgoStarting = 0.1
       ret.stoppingDecelRate = 0.05  # reach stopping target smoothly
     tune.kpBP = [0.,]
     tune.kiBP = [0., 3.]
