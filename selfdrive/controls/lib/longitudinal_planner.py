c898f28f283a2d7f1edbb8948c841c2d1b85cb87
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2024-12-17 22:14:54 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -24,7 +24,7 @@ from openpilot.selfdrive.controls.vtsc import vtsc
 
 LON_MPC_STEP = 0.2  # first step is 0.2s
 A_CRUISE_MIN = -1.2
-A_CRUISE_MAX_VALS = [1.12, 0.84, 0.8, 0.6] #[1.6, 1.2, 0.8, 0.6]
+A_CRUISE_MAX_VALS = [1.35, 1.1, 0.8, 0.6] #[1.12, 0.84, 0.8, 0.6] [1.6, 1.2, 0.8, 0.6]
 A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
 A_CRUISE_MIN_VALS =    [-1.02,  -0.25, -0.001, -0.01, -0.15, -0.25, -0.33, -0.42, -0.65, -0.65]
 A_CRUISE_MIN_BP =      [ 0.,    .01,   .02,    .3,     5.,    8.,    11.,   16.,   28.,   42.]
