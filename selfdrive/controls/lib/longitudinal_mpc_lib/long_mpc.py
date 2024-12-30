1eba021fa51a1fe4379b962da1360bbe9ab5ff75
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2024-12-30 16:58:13 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -61,11 +61,11 @@ COMFORT_BRAKE = 1.1 #2.5
 
 def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 1.0
+    return 1.2 #1.0
   elif personality==log.LongitudinalPersonality.standard:
-    return 0.5
+    return 0.6 #0.5
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 0.22
+    return 0.3 #0.22
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
@@ -84,14 +84,14 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
 def get_dynamic_follow(v_ego, personality=log.LongitudinalPersonality.standard):
   # The Dynamic follow function is adjusted by Marc(cgw1968-5779)
   if personality==log.LongitudinalPersonality.relaxed:
-    x_vel =  [0.0,  5.0,   13.90,  20,    25,    40]
-    y_dist = [1.2,  1.2,   1.5,   1.8,   2.2,  2.2]
+    x_vel =  [0.0,     9.7,     14,      28,       35]     #[0.0,  5.0,   13.90,  20,    25,    40]
+    y_dist = [1.05,    1.05,    1.15,    1.35,     1.4]    #[1.2,  1.2,   1.5,   1.8,   2.2,  2.2]
   elif personality==log.LongitudinalPersonality.standard:
-    x_vel =  [0.0,  5.0,   13.90,  20,    25,    40]
-    y_dist = [1.1,  1.1,   1.3,    1.45,  1.6,  1.6]
+    x_vel =  [0.0,     9.7,     14,      28,       35]     #[0.0,  5.0,   13.90,  20,    25,    40]
+    y_dist = [1.05,    1.05,    1.15,    1.35,     1.4]    #[1.1,  1.1,   1.3,    1.45,  1.6,  1.6]
   elif personality==log.LongitudinalPersonality.aggressive:
-    x_vel =  [0.0,  5.0,   12.00,  15.,   20,    25,    40]
-    y_dist = [1.05, 1.10,  1.20,   1.20,  1.25,  1.25,   1.3]
+    x_vel =  [0.0,     9.7,     14,      28,       35]     #[0.0,  5.0,   12.00,  15.,   20,    25,    40]
+    y_dist = [1.05,    1.05,    1.15,    1.35,     1.4]    #[1.05, 1.10,  1.20,   1.20,  1.25,  1.25,   1.3]
   else:
     raise NotImplementedError("Dynamic Follow personality not supported")
   return np.interp(v_ego, x_vel, y_dist)
