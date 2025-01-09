949b1744cf54a684f06d750ff65f5e1b1df39bd1
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-01-09 11:28:44 +0800

    更新 long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -99,11 +99,11 @@ def get_dynamic_follow(v_ego, personality=log.LongitudinalPersonality.standard):
 
 def get_STOP_DISTANCE(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 7.0
+    return 8.0
   elif personality==log.LongitudinalPersonality.standard:
-    return 7.0
+    return 8.0
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 7.0
+    return 8.0
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
