3096d38b910c2bc5990d61f894028d0ca4c89db3
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-01-27 11:08:21 +0800

    更新 long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -59,11 +59,11 @@ STOP_DISTANCE = 6.0
 
 def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 5.0 #1.5
+    return 6.0 #1.5
   elif personality==log.LongitudinalPersonality.standard:
-    return 4.5
-  elif personality==log.LongitudinalPersonality.aggressive:
     return 4.0
+  elif personality==log.LongitudinalPersonality.aggressive:
+    return 2.0
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
@@ -74,7 +74,7 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   elif personality==log.LongitudinalPersonality.standard:
     return 1.35
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 1.45
+    return 1.55
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
@@ -92,7 +92,7 @@ def get_stopped_equivalence_factor(v_lead, v_ego):
   #delta_speed = 0.0 if abs(v_lead) < 0.5 else v_lead - v_ego
   if np.all(delta_speed > 0.5):
     #v_diff_offset = delta_speed * 40 #2
-    v_diff_offset = delta_speed**5 #4
+    v_diff_offset = delta_speed**6 #4
     v_diff_offset = np.clip(v_diff_offset, 0, v_diff_offset_max)
 
     v_diff_offset = np.maximum(v_diff_offset * ((speed_to_reach_max_v_diff_offset - v_ego)/speed_to_reach_max_v_diff_offset), 0)
