cae90e115cbe6585e0ecd8760fe852e6deb09108
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2026-04-20 10:25:31 +0800

    更新 longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -82,9 +82,9 @@ PREBRAKE_MAX_DECEL_BASE = -1.6
 
 LON_MPC_STEP = 0.2  # first step is 0.2s
 
-A_CRUISE_MAX_VALS = [0.6, 0.75, 0.805, 0.644, 0.441, 0.198]
+A_CRUISE_MAX_VALS = [0.6,  0.7,   0.8,   0.805, 0.644, 0.441, 0.198]
 #A_CRUISE_MAX_VALS = [1.00, 1.085, 0.805, 0.644, 0.441, 0.245, 0.198]
-A_CRUISE_MAX_BP   = [0.0,  8.33,  15.0,  20.0,  25.0,  30.0]
+A_CRUISE_MAX_BP   = [0.0,  2.78,  8.33,  15.0,  20.0,  25.0,  30.0]
 
 CONTROL_N_T_IDX = ModelConstants.T_IDXS[:CONTROL_N]
 
